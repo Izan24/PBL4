bebbff853846287f0264e1a786e42d563653f07d
UrkoAT <urko.agirregomezkort@alumni.mondraon.edu>
2021-06-07 16:38:30 +0200

    ApiRequests

--- src/eus/cic/core/app/utils/APIRequests.java
+++ src/eus/cic/core/app/utils/APIRequests.java
@@ -17,8 +17,35 @@ public class APIRequests {
 		JSONObject response = APIutils.getRequest("/api/getBuildings?session=" + SessionHandler.getSession());
 		if (response.getString("status").equals("ok")) {
 			buildings = JSONParser.parseBuildings(response.getJSONArray("buildings"));
+		}else {
+			throw new SessionException();
 		}
 		return buildings;
+	}
+	
+	public static Integer insertBuilding(Building building) throws SessionException {
+		Integer id = -1;
+		JSONObject json = new JSONObject().put("name", building.getNombre()).put("postal_code", building.getPostalCode());
+		json.put("disable", false).put("session", json.put("session", SessionHandler.getSession()));
+		JSONObject response = APIutils.postRequest("/api/insertBuilding", json);
+		if (response.getString("status").equals("ok")) {
+			id = response.getInt("id");
+		}else {
+			throw new SessionException();
+		}
+		return id;
+	}
+	
+	public static Integer disableBuilding(Building building) throws SessionException {
+		Integer id = -1;
+		JSONObject json = new JSONObject().put("name", building.getNombre()).put("postal_code", building.getPostalCode());
+		JSONObject response = APIutils.postRequest("/api/insertBuilding", json.put("session", SessionHandler.getSession()));
+		json.put("disable", true).put("session", json.put("session", SessionHandler.getSession()));
+		if (response.getString("status").equals("ok")) {
+		}else {
+			throw new SessionException();
+		}
+		return id;
 	}
 	
 	
@@ -29,6 +56,8 @@ public class APIRequests {
 				.getRequest("/api/getUser?id=" + user_id + "&session=" + SessionHandler.getSession());
 		if (response.getString("status").equals("ok")) {
 			user = JSONParser.parseUser((response.getJSONObject("user")));
+		}else {
+			throw new SessionException();
 		}
 		return user;
 	}
