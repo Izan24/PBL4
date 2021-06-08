43066ce11bc461481647547a46ac97ca0cf9310c
heiko <heiko@DESKTOP-IDN3683.mshome.net>
2021-06-08 17:31:19 +0200

    wdefgrtryyiuoip

--- src/eus/cic/core/app/admin/ui/menu/AdminMenuUI.java
+++ src/eus/cic/core/app/admin/ui/menu/AdminMenuUI.java
@@ -19,7 +19,7 @@ import eus.cic.core.app.admin.controller.menu.AdminMenuController;
 public class AdminMenuUI extends JPanel {
 
 	private static final Color BG_COLOR = Color.white;
-	
+
 	AdminMenuController controller;
 
 	public AdminMenuUI() {
@@ -27,47 +27,44 @@ public class AdminMenuUI extends JPanel {
 		this.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 		controller = new AdminMenuController(this);
 		this.setBackground(BG_COLOR);
-		this.setOpaque(true);	
-		
+		this.setOpaque(true);
+
 		addbuttonsPanel();
-		
+
 	}
 
 	private void addbuttonsPanel() {
-		this.add (createButton("Edificios", controller, Color.white), setConstraints(1,1));
-		this.add (createButton("Salas", controller, Color.white), setConstraints(1,3));
-		this.add (createButton("Trabajadores", controller, Color.white), setConstraints(1,5));
-		this.add (createButton("Proyectos", controller, Color.white), setConstraints(3,1));
-		this.add (createButton("Tarjetas", controller, Color.white), setConstraints(3,3));
-		this.add (createButton("Reservas", controller, Color.white), setConstraints(3,5));
-		this.add (createButton("Permisos", controller, Color.white), setConstraints(5,1));
-		this.add (createButton("Fichas", controller, Color.white), setConstraints(5,3));
-		this.add (createButton("Bonos", controller, Color.white), setConstraints(5,5));
-		
+		this.add(createButton("Edificios", controller, Color.white), setConstraints(1, 1));
+		this.add(createButton("Salas", controller, Color.white), setConstraints(1, 3));
+		this.add(createButton("Trabajadores", controller, Color.white), setConstraints(1, 5));
+		this.add(createButton("Proyectos", controller, Color.white), setConstraints(3, 1));
+		this.add(createButton("Tarjetas", controller, Color.white), setConstraints(3, 3));
+		this.add(createButton("Reservas", controller, Color.white), setConstraints(3, 5));
+		this.add(createButton("Permisos", controller, Color.white), setConstraints(5, 1));
+		this.add(createButton("Fichas", controller, Color.white), setConstraints(5, 3));
+		this.add(createButton("Bonos", controller, Color.white), setConstraints(5, 5));
+
 	}
 
 	private GridBagConstraints setConstraints(int gridx, int gridy) {
 		GridBagConstraints constraints = new GridBagConstraints();
-		//constraints.fill = GridBagConstraints.HORIZONTAL;
 		constraints.weighty = 1;
 		constraints.weightx = 1;
 		constraints.gridx = gridx;
 		constraints.gridy = gridy;
 		constraints.weighty = 1.0;
-		//constraints.weightx = 0.0;
-		
+
 		return constraints;
 	}
 
-	private Component createButton(String name, ActionListener controller ,Color color) {
+	private Component createButton(String name, ActionListener controller, Color color) {
 		JButton boton = new JButton(name);
 		boton.addActionListener(controller);
 		boton.setActionCommand(name);
 		boton.setBackground(color);
 		boton.setFocusable(false);
-		boton.setPreferredSize(new Dimension(150,150));
+		boton.setPreferredSize(new Dimension(150, 150));
 		return boton;
 	}
-
 
 }
